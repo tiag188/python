56df3ccb1a6af26c0f52d1b6a1a782598278b17d
Tiago Silva <70734516+tiag188@users.noreply.github.com>
2023-04-12 23:09:02 -0300

    feat: Olá mundo!

---
+++ app.py
@@ -0,0 +1,11 @@
+valor = "python"
+separar_por_underline = "Hello World"
+nome = "Tiag"
+idade = "34 anos"
+preferencia = "Programação"
+profissao = "Humano"
+
+print(valor)
+print(separar_por_underline)
+print(f"Eu sou {nome}")
+print(f"Eu sou {nome} e eu tenho {idade} e possuo preferencia por {preferencia}, atualmente minha profissão é {profissao}")
