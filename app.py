9eddbcac7879e11b042f73df1021c37655d6ecd9
Tiago Silva <70734516+tiag188@users.noreply.github.com>
2023-04-12 23:43:31 -0300

    feat: funcionalidades gerais

--- app.py
+++ app.py
@@ -1,11 +1,56 @@
 valor = "python"
 separar_por_underline = "Hello World"
 nome = "Tiag"
-idade = "34 anos"
+idade = 34
+altura = 1.77
+ativo = True
 preferencia = "Programação"
 profissao = "Humano"
+
+print(type(nome))
+print(type(idade))
+
+print(type(altura))
+print(type(ativo))
 
 print(valor)
 print(separar_por_underline)
 print(f"Eu sou {nome}")
-print(f"Eu sou {nome} e eu tenho {idade} e possuo preferencia por {preferencia}, atualmente minha profissão é {profissao}")
+print(f"Eu sou {nome} e eu tenho {idade} anos e possuo preferencia por {preferencia}, atualmente minha profissão é {profissao} e está {ativo}")
+
+a = float (idade)
+print(a)
+print(type(a))
+
+b = int(altura)
+print(b)
+print(type(b))
+
+num1 = 9
+num2 = 4
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
+print(num1 ** num2)
+
+print((3 + 5) * (7 + 3))
+
+print(abs(-188))
+print(pow(3,3))
+print(max(17,54, 28,35, -25, 44))
+print(min(17,54, 28,35, -25, 44))
+
+print(round(8.8))
+print(round(8.4))
+
+import math
+
+print(math.floor(222.222))
+print(math.floor(222.8))
+print(math.ceil(222.222))
+print(math.ceil(222.8))
+
+print(math.sqrt(9))
